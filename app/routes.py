8801540288329e5cc90849bbd821cfd67f815d0d
Jonathan Mucha <jonmucha@gmail.com>
2020-01-09 13:16:23 -0500

    small changes to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from app import app, nav, db
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from flask_nav.elements import Navbar, View
 from app.forms import RunTrafficForm, AddPlatformUrlForm, AddPlatformForm
 from app.models import IntegrationPlatform, Url
 from app.browser_bot import BrowserBot
-
+import sys
 
 nav.register_element('demo_traffic', Navbar(
     View('Home', '.index'),
@@ -14,9 +14,13 @@ nav.register_element('demo_traffic', Navbar(
 ))
 
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    bb_list = []
+    print(sys.getsizeof(bb_list) / 1024.0)
     form = RunTrafficForm()
     if request.method == "POST" and form.validate_on_submit():
         platform = form.platform_select_field.data
@@ -27,12 +31,14 @@ def index():
                 window_urls.append(url.address)
             for num in range(url.num_headless):
                 # headless_urls.append(url.address)
-                BrowserBot(url=url.address)
+                bb =BrowserBot(url=url.address)
+                print(sys.getsizeof(bb) / 1024.0)
+                bb_list.append(bb)
+        # print(sys.getsizeof(bb_list) / 1024.0)
         return render_template('index.html', form=form, window_urls=window_urls)
 
-
-
     return render_template('index.html', form=form)
+
 
 @app.route('/add_integration', methods=['GET', 'POST'])
 def add_integration():
@@ -42,10 +48,10 @@ def add_integration():
         platform_name = platform_form.platform_name.data
         # print(platform_name)
         if len(IntegrationPlatform.query.filter_by(platform=platform_name).all()) == 0:
-            new_platform = IntegrationPlatform(platform = platform_name)
+            new_platform = IntegrationPlatform(platform=platform_name)
             db.session.add(new_platform)
             db.session.commit()
-        return(redirect(url_for('add_integration')))
+        return (redirect(url_for('add_integration')))
     elif request.method == 'POST' and url_form.validate_on_submit():
         url = url_form.url_field.data
         if not url_form.num_headless:
@@ -58,11 +64,18 @@ def add_integration():
             num_windows = url_form.num_plain.data
         platform = url_form.platform_select_field.data
         if len(Url.query.filter_by(address=url).all()) == 0:
-            new_url = Url(address = url, num_headless=num_headless, num_windows=num_windows)
+            new_url = Url(address=url, num_headless=num_headless, num_windows=num_windows)
             db.session.add(new_url)
             platform.urls.append(new_url)
             db.session.commit()
         return (redirect(url_for('add_integration')))
 
-    return render_template('add_integration.html', platform_form=platform_form, url_form = url_form)
+    return render_template('add_integration.html', platform_form=platform_form, url_form=url_form)
 
+
+# @app.route('/_free_mem')
+# def _free_mem():
+#     print(sys.getsizeof(session['bb_list']) / 1024.0)
+#     bb_session['bb_list'] = []
+#     print(sys.getsizeof(session['bb_list']) / 1024.0)
+#     return redirect(url_for('index'))
