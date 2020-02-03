443ec972308a40af273a191cfbb7c9a469bd1370
Jonathan Mucha <jonmucha@gmail.com>
2020-02-03 10:02:15 -0500

    adjusted the length of the password string in db

--- app/models.py
+++ app/models.py
@@ -17,8 +17,8 @@ class Url(db.Model):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(64), index=True, unique=True)
+    user_name = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(256), index=True, unique=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
